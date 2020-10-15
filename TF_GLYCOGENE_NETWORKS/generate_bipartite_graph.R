b4b3c7ed15ba502ed323d357e1382e684511712b
tgroth58 <tgroth@buffalo.edu>
2020-10-15 01:36:47 -0400

    Updated Readme.txt

---
+++ TF_GLYCOGENE_NETWORKS/generate_bipartite_graph.R
@@ -0,0 +1,54 @@
+library(dplyr)
+library(tidyr)
+library(igraph)
+load('../data/processed/merge_data_glycogenes.rda')
+load('../data/Glycogene_list.rda')
+load('../data/pathLists.rda')
+source('./get_reactome_table.R')
+source('./hyper_geo_test_tfList.R')
+
+# Input:
+args=commandArgs(trailingOnly=T)
+ctype_input=args[1]
+
+get_bipartite_graph<-function(df){
+	#Get rid of OGT occurrence as a TF, because it is a glycogene:
+	df<-df %>% filter(TF!='OGT')
+	gph<-graph.data.frame(df,directed=F)
+	V(gph)$type=V(gph)$name %in% df$TF
+	#E(gph)$weight=abs(df$BP)
+	#Create bipartite projection:
+	gph_proj=bipartite_projection(gph)
+	#Cluster the TFs based on the bipartite projection:
+	TF_clust=fastgreedy.community(gph_proj$proj2)
+	#Return things as a list:
+	return(list('bp_graph'=gph,'TF_projection'=gph_proj$proj2,'clusters'=TF_clust))
+}
+
+input_net_data=merge_data_glycogenes %>% filter(RP>=0.5,cor>=0.4,ctype==ctype_input)
+
+graphClust=get_bipartite_graph(input_net_data)
+#Membership vector:
+mem_vec=membership(graphClust$clusters)
+#Merge the membership data with the input data matrix:
+mem_df=data.frame(TF=names(mem_vec),clst=as.numeric(mem_vec))
+clust_net_data<-input_net_data %>% inner_join(mem_df,by='TF') %>% mutate(BP=RP*cor)
+#For each membership, find enrichment to glycogene pathways and signaling pathways from reactome.
+enrichments<-lapply(unique(mem_vec),function(cl){
+	tfs=names(mem_vec[which(mem_vec==cl)])
+	if (length(tfs)<5){
+		return(NULL)
+	} else {
+		#Glycogene pathway enrichments:
+		gg_enrich<-lapply(names(pathLists),function(x) path_hgt_list(input_net_data,pathLists[[x]],tfs))
+		names(gg_enrich)=names(pathLists)
+		#Reactome TF enrichment:
+		tf_enrich<-get_reactome_enrich_table(tfs)
+		return(list('ggenes'=gg_enrich,'tfs'=tf_enrich))
+	}
+})
+
+graphClust[['enrichments']]=enrichments
+
+save(file=file.path('TF_glycogene_graphs',paste(ctype_input,'communities.rda',sep='_')),graphClust)
+write.table(file=file.path('TF_glycogene_graphTables',paste(ctype_input,'clustTable.tsv',sep='_')),sep='\t',row.names=F,quote=F,clust_net_data)
---
+++ TF_GLYCOGENE_NETWORKS/hyper_geo_test_tfList.R
@@ -0,0 +1,17 @@
+path_hgt_list<-function(gene_TF_data,pathList,tf_list){
+	#Variable in the "gene_TF_data" variable is cancer type:
+	#Contingency table:
+	mtx<-matrix(rep(0,4),nrow=2)
+	#Build Contingency Table:	
+	mtx[1,1]=nrow( gene_TF_data %>% filter(TF %in% tf_list & gene %in% pathList) )
+	mtx[1,2]=nrow( gene_TF_data %>% filter(TF %in% tf_list & !(gene %in% pathList)) )
+	mtx[2,1]=nrow( gene_TF_data %>% filter(!(TF %in% tf_list) & gene %in% pathList) ) 
+	mtx[2,2]=nrow( gene_TF_data)-sum(c(mtx[1,1],mtx[1,2],mtx[2,1]) )
+	#Format mtx :
+	mtx<-as.table(mtx)
+	#HyperGeoTest :
+	hgt<-fisher.test(mtx,alternative='greater')$p.value
+	#Return HyperGeoTest test p-value:
+	return(hgt)
+}
+
